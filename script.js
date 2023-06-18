5a8a02d9e55efd16190f7a52e2e7993b7b5fab7f
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-06-18 01:32:11 -0400

    able to get city weather

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fetchWeather(cityName) {
     var weatherKey = '96515e32da8200e2651c60008ed403ea';
     var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherKey}`;
     
-
+// Fetching and processing data from weatherURL, which is pulling data from Openweathermaps
     fetch(weatherURL)
       .then(response => response.json())
       .then(data => {
@@ -12,7 +12,7 @@ function fetchWeather(cityName) {
         var temperature = data.main.temp;
         var humidity = data.main.humidity;
         var windSpeed = data.wind.speed;
-  
+
         document.getElementById("city-name").textContent = name;
         document.getElementById("date").textContent = date;
         document.getElementById("weather-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}.png" alt="Weather Icon">`;
